5cbc89b40f09674d81a66b1edc52b47bbfdb06f2
Alexandr Anfreev <1996sasha2507@mail.ru>
2019-02-11 20:49:59 +0300

    Добалены генераторы случаных данных для тестов создания контактов и групп

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,29 +1,26 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 import pytest
+import random
+import string
 
 
-testdata = [
-    Contact(firstname="asdasd", middlename="asd", lastname="fddfdf", nickname="fdfddffd",
-                               title="adfadf", company="asdfafd", homephone="634850", workphone="89679185042"),
-    Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", homephone="", workphone="")
+def random_string(prefix:str, maxlen:int):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname="", middlename="", lastname="", nickname="", title="", company="",
+                    homephone="", workphone="")] + [
+            Contact(firstname="asdasd", middlename="asd", lastname="fddfdf", nickname="fdfddffd", title="adfadf",
+                    company="asdfafd", homephone="634850", workphone="89679185042")
+            for i in range(5)
 ]
 
 
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
 def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    app.contact.create(contact)
-    assert len(old_contacts) + 1 == app.contact.count()
-    new_contacts = app.contact.get_contact_list()
-    old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-def test_add_empty_contact(app):
-    old_contacts = app.contact.get_contact_list()
-    contact = (Contact(firstname="", middlename="", lastname="", nickname="",
-                               title="", company="", homephone="", workphone=""))
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
