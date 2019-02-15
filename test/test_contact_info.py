e36d437c767b5d0eb6d26987c9daa7427fece7a5
Alexandr Anfreev <1996sasha2507@mail.ru>
2019-02-15 19:11:33 +0300

    Исправлено задание с адресом

--- test/test_contact_info.py
+++ test/test_contact_info.py
@@ -40,6 +40,7 @@ def test_personal_info_home_page(app):
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
+    assert contact_from_home_page.address == contact_from_edit_page.address
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
 
 
@@ -54,3 +55,4 @@ def test_personal_info_on_contact_view_page(app):
     assert contact_from_view_page.nickname == contact_from_edit_page.nickname
     assert contact_from_view_page.title == contact_from_edit_page.title
     assert contact_from_view_page.company == contact_from_edit_page.company
+    assert contact_from_view_page.address == contact_from_edit_page.address
