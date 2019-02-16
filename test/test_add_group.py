82236ded6c5592d656459378009bed611b69974c
Alexandr Anfreev <1996sasha2507@mail.ru>
2019-02-16 20:57:26 +0300

    Исходные данные вынесены в отдельный файл

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,19 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
-import random
-import string
-
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-    for i in range(10)
-]
+from data.add_group import testdata
 
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
