1e9ef920fead441d99934676149462b5e847296a
Alexandr Anfreev <1996sasha2507@mail.ru>
2019-02-20 18:55:22 +0300

    Загрузка данных из файла json

---
+++ data/contacts.py
@@ -0,0 +1,10 @@
+from model.contact import Contact
+
+
+testdata = [Contact(firstname="firstname1", middlename="middlename1", lastname="lastname1",
+                    nickname="nickname1", title="title1", company="company1", address="address1",
+                    homephone="899393939", workphone="933999"),
+            Contact(firstname="firstname2", middlename="middlename2", lastname="lastname2",
+                    nickname="nickname2", title="title2", company="company2", address="address2",
+                    homephone="89939393259", workphone="933999325")
+            ]
