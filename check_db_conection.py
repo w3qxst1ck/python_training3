982ff58b81e2f94861000fbbcb5ab08144ba6d80
Alexandr Anfreev <1996sasha2507@mail.ru>
2019-02-25 18:52:17 +0300

    Не работает метод get_contacts_in_group

--- check_db_conection.py
+++ check_db_conection.py
@@ -1,14 +1,16 @@
-import pymysql.cursors
+from fixture.db import DbFixture
+from model.group import Group
+from fixture.orm import ORMFixture
 
-connection = pymysql.connect(host='127.0.0.1', database="addressbook", user="root", password="")
+db = ORMFixture(host='127.0.0.1', name="addressbook", user="root", password="")
 
 try:
-    cursor = connection.cursor()
-    cursor.execute("select * from group_list")
-    for row in cursor.fetchall():
-        print(row)
+    l = db.get_contacts_in_group(Group(id='90'))
+    for item in l:
+        print(item)
+    print(len(l))
 finally:
-    connection.close()
+    pass  # db.destroy()
 
 
 
--- fixture/db.py
+++ fixture/db.py
@@ -1,5 +1,6 @@
 import pymysql.cursors
 from model.group import Group
+from model.contact import Contact
 
 
 class DbFixture:
@@ -10,6 +11,18 @@ class DbFixture:
         self.user = user
         self.password = password
         self.connection = pymysql.connect(host=host, database=name, user=user, password=password, autocommit=True)
+
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, firstname, lastname) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
+        finally:
+            cursor.close()
+        return list
 
     def get_group_list(self):
         list = []
